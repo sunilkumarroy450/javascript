5f8837c4643da2c0e59e6c16bd8385243acc5796
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-03-17 02:26:20 +0530

    arrays and strings letcode sol

--- string.js
+++ string.js
@@ -4,3 +4,20 @@ const defangIPaddr = function (address) {
 };
 
 defangIPaddr("1.1.1.1");
+
+//Q2
+
+var finalValueAfterOperations = function (operations) {
+  let X = 0;
+
+  for (let i = 0; i < operations.length; i++) {
+    if (operations[i] === "X++" || operations[i] === "++X") {
+      X++;
+    } else {
+      X--;
+    }
+  }
+  return X;
+};
+const res1 = finalValueAfterOperations(["--X", "X++", "X++"]);
+console.log(res1);
