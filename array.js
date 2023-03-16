fd6b53fbb9b0b652e5bb991b86394f3a3e236904
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-03-16 22:08:01 +0530

    array letcode sol

--- array.js
+++ array.js
@@ -3,11 +3,34 @@ const buildArray = function (nums) {
   const ans = new Array(n);
   for (let i = 0; i < n; i++) {
     ans[i] = nums[nums[i]];
-    console.log(ans[i], "ans[i] 2");
   }
-console.log(ans,"ans outside")
   return ans;
 };
 
 const arr = [0, 2, 1, 5, 3, 4];
 console.log(buildArray(arr));
+
+//Q2
+
+var getConcatenation1 = function (nums) {
+  const ans = [...nums, ...nums];
+  return ans;
+};
+
+const res1 = getConcatenation1([1, 2, 1]);
+console.log(res1);
+
+// OR
+
+var getConcatenation = function (nums) {
+  let n = nums.length;
+  let ans = new Array(2n);
+  for (let i = 0; i < n; i++) {
+    ans[i] = nums[i];
+    ans[i + n] = nums[i];
+  }
+  return ans;
+};
+
+const res = getConcatenation([1, 2, 1]);
+console.log(res);
