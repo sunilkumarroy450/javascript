37dbd3d95033ff913ba774492e32e2d499717c19
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-05-09 18:24:17 +0530

    implementing stack

---
+++ daily-dsa/7.js
@@ -0,0 +1,42 @@
+// Implementing stack
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+// Usage
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.pop()); // Output: 3
+console.log(stack.peek()); // Output: 2
+console.log(stack.isEmpty()); // Output: false
+console.log(stack.size()); // Output: 2
